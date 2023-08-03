19e47d04e816fc285c8243aee52674a478fcc34f
Ciaran Bench <53267034+ciaranbench@users.noreply.github.com>
2023-08-03 14:08:10 +0100

    added comment

--- training_notebooks/spectral_cycleGAN_pytorch.py
+++ training_notebooks/spectral_cycleGAN_pytorch.py
@@ -1,3 +1,5 @@
+# This code is modified from https://www.kaggle.com/code/pipiking/cyclegan-pytorch/script
+
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load in 
